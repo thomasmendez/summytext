422c03c5421795652d3d6d2d1074479b70062b8c
thomasmendez <thomasmendez01@gmail.com>
2022-11-23 01:04:14 -0600

    Fixed webpack eslint issues

--- frontend/webpack.prod.js
+++ frontend/webpack.prod.js
@@ -12,7 +12,19 @@ module.exports = {
     rules: [
       {
         test: /\.(jsx|js)$/,
-        exclude: '/node_modules/',
+        include: path.resolve(__dirname, 'node_modules'),
+        use: ['babel-loader'],
+      },
+      {
+        test: /\.(png|jpe?g|gif|mp4|pdf)$/i,
+        loader: 'file-loader',
+        options: {
+          name: '[path][name].[ext]',
+        },
+      },
+      {
+        test: /\.(jsx|js)$/,
+        include: [path.resolve(__dirname, './src')],
         use: ['babel-loader', 'eslint-loader'],
       },
     ],
@@ -32,7 +44,7 @@ module.exports = {
       ignore: true,
       useEslintrc: true,
       extensions: ['.js', '.jsx'],
-      exclude: 'node_modules',
+      files: [path.join(__dirname, 'src')],
       fix: true,
       emitError: true,
       failOnError: false,
