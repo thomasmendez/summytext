66e8117460392f6f116287e1cb855c5d9486788f
thomasmendez <thomasmendez01@gmail.com>
2022-11-22 23:42:35 -0600

    Added analysis view

---
+++ frontend/src/components/CardResult/CardResult.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Card, CardContent, Typography } from '@mui/material';
+
+const CardResult = (props) => {
+  const { backgroundColor, title, content } = props;
+    return (
+      <Card sx={{ width: 1 }} style={{ backgroundColor: backgroundColor }}>
+        <CardContent>
+          <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+            {title}
+          </Typography>
+          <Typography variant="h5" component="div">
+            {content}
+          </Typography>
+        </CardContent>
+      </Card>
+    );
+};
+
+CardResult.defaultProps = {
+  backgroundColor: 'lavender',
+  title: 'Title',
+  content: 'Content',
+};
+
+CardResult.propTypes = {
+  backgroundColor: PropTypes.string,
+  title: PropTypes.string,
+  content: PropTypes.string
+};
+
+export default CardResult;
