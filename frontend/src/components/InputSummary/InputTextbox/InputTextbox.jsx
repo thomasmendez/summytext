09b150ee883085812f8a90f5a4d5a04237a0a495
Thomas Mendez <thomasmendez01@gmail.com>
2022-11-27 01:06:12 -0600

    Added redux for input text box for on submit

--- frontend/src/components/InputSummary/InputTextbox/InputTextbox.jsx
+++ frontend/src/components/InputSummary/InputTextbox/InputTextbox.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
+import { analysisActions } from '../../../store/analysis';
 import PropTypes from 'prop-types';
 import { Card, CardContent, CardActions, TextField } from '@mui/material';
 import SubmitButton from './SubmitButton';
 
 const InputTextbox = (props) => {
   const { backgroundColor } = props;
+  const dispatch = useDispatch();
   return(
     <Card sx={{ width: 1 }} style={{ backgroundColor: backgroundColor }}>
       <CardContent>
@@ -13,6 +16,7 @@ const InputTextbox = (props) => {
           label="Enter text you wish to summarize here..."
           multiline
           rows={20}
+          onChange={(e) => dispatch(analysisActions.handleTextFieldChange(e.target.value))}
         />
       </CardContent>
       <CardActions>
--- frontend/src/components/InputSummary/InputTextbox/SubmitButton/SubmitButton.jsx
+++ frontend/src/components/InputSummary/InputTextbox/SubmitButton/SubmitButton.jsx
@@ -8,6 +8,7 @@ const SubmitButton = () => {
 
   const dispatch = useDispatch();
 
+  const text = useSelector((state) => state.analysis.text);
   const isLoading = useSelector((state) => state.analysis.isLoading);
 
   return(
@@ -21,7 +22,7 @@ const SubmitButton = () => {
           Submit
         </LoadingButton>
       ) : (
-        <Button fullWidth variant="contained" color="success" size='large' onClick={() => {dispatch(analysisActions.performAnalysis({text: 'my text'}));}}>
+        <Button fullWidth variant="contained" color="success" size='large' onClick={() => {dispatch(analysisActions.performAnalysis({text: text}));}}>
           Submit
         </Button>
       )}
