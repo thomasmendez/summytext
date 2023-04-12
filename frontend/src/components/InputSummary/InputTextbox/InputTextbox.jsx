73f28bb2c67397b17bcb1d39726f9c3eb4b5c71a
thomasmendez <thomasmendez01@gmail.com>
2023-04-11 23:37:14 -0500

    Submitted text is preserved in store

--- frontend/src/components/InputSummary/InputTextbox/InputTextbox.jsx
+++ frontend/src/components/InputSummary/InputTextbox/InputTextbox.jsx
@@ -12,6 +12,7 @@ const InputTextbox = (props) => {
   const { backgroundColor } = props;
   const dispatch = useDispatch();
   const text = useSelector((state) => state.analysis.text);
+  const previousText = useSelector((state) => state.analysis.previousText);
   const { browserSupportsSpeechRecognition } = useSpeechRecognition();
   if (!browserSupportsSpeechRecognition) {
     console.warn('Browser does not support speech recognition');
@@ -24,7 +25,7 @@ const InputTextbox = (props) => {
           label="Enter text you wish to summarize here..."
           multiline
           rows={20}
-          value={text || ''}
+          value={text || previousText || ''}
           inputRef={input => input && input.focus()}
           InputLabelProps={{ shrink: true }}
           onChange={(e) => dispatch(analysisActions.handleTextFieldChange(e.target.value))}
