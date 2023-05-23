b554b7de72f0664833851132234cc5ca57bf488a
thomasmendez <thomasmendez01@gmail.com>
2023-05-23 00:15:47 -0500

    Added concurrency

--- backend/app/api/api_v1/endpoints/predict.py
+++ backend/app/api/api_v1/endpoints/predict.py
@@ -1,3 +1,5 @@
+import asyncio
+import time
 from fastapi import APIRouter
 from pydantic import BaseModel
 from summarizer import TransformerSummarizer
@@ -16,11 +18,11 @@ router = APIRouter()
 class InputText(BaseModel):
     text: str
 
-def predict_summary(text: str) -> str:
+async def predict_summary(text: str) -> str:
     summary = ''.join(summarizer_transformer(text, min_length=60))
     return summary
 
-def predict_sentiment(text: str):
+async def predict_sentiment(text: str):
     
     sentence = Sentence(text)
 
@@ -28,7 +30,7 @@ def predict_sentiment(text: str):
 
     return sentence.labels[0].value
 
-def predict_topics(text: str):
+async def predict_topics(text: str):
 
     sentence = Sentence(text)
 
@@ -45,14 +47,55 @@ def predict_topics(text: str):
 
     return topic_labels, topics, labels
 
+async def process_predictions(text):
+    tasks = []
+    results = []
+
+    task = asyncio.create_task(predict_summary(text))
+    tasks.append(task)
+
+    task = asyncio.create_task(predict_sentiment(text))
+    tasks.append(task)
+
+    task = asyncio.create_task(predict_topics(text))
+    tasks.append(task)
+    
+    # Await the completion of all tasks
+    results = await asyncio.gather(*tasks)
+
+    return results
+
 @router.post("/")
 async def analyze(input_text: InputText):
 
-    topic_labels, topics, labels = predict_topics(input_text.text)
+    # Measure execution time without concurrency
+    # start_time = time.time()
+    # summary = predict_summary(input_text.text)
+    # sentiment = predict_sentiment(input_text.text)
+    # topic_labels, topics, labels = predict_topics(input_text.text)
+    # end_time = time.time()
+    # execution_time_without_concurrency = end_time - start_time
+    # print("execution time: {0}s".format(execution_time_without_concurrency))
+
+    # Measure execution time with concurrency
+    start_time = time.time()
+    
+    results = await process_predictions(input_text.text)
+    # for value in results:
+    #     print(f"Value: {value}")
+    summary = results[0]
+    sentiment = results[1]
+    topic_labels = results[2][0]
+    topics = results[2][1]
+    labels = results[2][2]
+
+    end_time = time.time()
+    execution_time_with_concurrency = end_time - start_time
+    print("execution time: {0}s".format(execution_time_with_concurrency))
 
     return {
-        'summary': predict_summary(input_text.text),
-        'sentiment': predict_sentiment(input_text.text),
+        'summary': summary,
+        'sentiment': sentiment,
         'topic_labels': topic_labels,
         'topics': topics,
         'labels': labels,
