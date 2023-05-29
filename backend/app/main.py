de6c55b44f84e7a5ae51a60fbaa523e7b07c9a7b
thomasmendez <thomasmendez01@gmail.com>
2023-05-29 02:36:47 -0500

    Added models to image

--- backend/app/main.py
+++ backend/app/main.py
@@ -3,38 +3,40 @@ from fastapi.middleware.cors import CORSMiddleware
 from dotenv import load_dotenv
 import os
 
+# os.environ['HF_HOME'] = './transformers/cache/'
+
 from summarizer import TransformerSummarizer
 from flair.nn import Classifier
+
+summarizer_transformer = TransformerSummarizer(transformer_type='GPT2',transformer_model_key='gpt2-medium')
+sentiment_classifier = Classifier.load('sentiment')
+topic_labels_classifier = Classifier.load('ner-ontonotes-large')
 
 from app.api.api_v1.api import router as api_router
 from mangum import Mangum
 
 load_dotenv()
-env = os.getenv("ENV")
+env = os.getenv('ENV')
 
 app = FastAPI()
 
 origins = []
 
-if env != None and env != "local":
+if env != None and env != 'local':
     print(env)
-    app.root_path = f"/{env}"
+    app.root_path = f'/{env}'
 
-if env == "local":
-    origins.append("http://localhost")
-    origins.append("http://localhost:8080")
+if env == 'local':
+    origins.append('http://localhost')
+    origins.append('http://localhost:8080')
 
 app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
     allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
+    allow_methods=['*'],
+    allow_headers=['*'],
 )
 
-summarizer_transformer = TransformerSummarizer(transformer_type="GPT2",transformer_model_key="gpt2-medium")
-sentiment_classifier = Classifier.load('sentiment')
-topic_labels_classifier = Classifier.load('ner-ontonotes-large')
-
-app.include_router(api_router, prefix="/api/v1")
+app.include_router(api_router, prefix='/api/v1')
 handler = Mangum(app)
