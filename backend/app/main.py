574c08fcff1960bbe81c9cb87ec2db89db64db4b
thomasmendez <thomasmendez01@gmail.com>
2023-06-19 23:42:53 -0500

    Added rate limit for api calls

--- backend/app/main.py
+++ backend/app/main.py
@@ -5,6 +5,7 @@ import os
 
 load_dotenv()
 env = os.getenv('ENV')
+proxy = os.getenv('PROXY')
 import flair
 from pathlib import Path
 
@@ -26,7 +27,7 @@ app = FastAPI()
 
 origins = []
 
-if env != None and env != 'local':
+if env != None and env != 'local' and proxy == 'true':
     app.root_path = f'/{env}'
 
 if env == 'local':
@@ -49,6 +50,69 @@ app.add_middleware(
     allow_methods=['*'],
     allow_headers=['*'],
 )
+
+from fastapi.responses import JSONResponse
+from cachetools import LRUCache
+import asyncio
+import time
+
+cache = LRUCache(maxsize=500)  # Adjust the cache size as needed
+
+RATE_LIMIT = 60  # Maximum number of requests allowed per hour
+CACHE_EXPIRATION = 3600  # Expiration time for cache entries in seconds
+
+@app.middleware("http")
+async def cache_requests(request, call_next):
+    ip_address = request.client.host
+
+    # Get the current timestamp
+    current_time = time.time()
+
+    if ip_address not in cache:
+        # If the IP address is not in the cache, initialize it with the request count and the time of first access
+        cache[ip_address] = {"request_count": 1, "first_access_time": current_time}
+    else:
+        # If the IP address is already in the cache, update the request count and check the rate limit
+        cache[ip_address]["request_count"] += 1
+
+        # Check if the rate limit has been exceeded
+        if cache[ip_address]["request_count"] > RATE_LIMIT:
+            elapsed_time = current_time - cache[ip_address]["first_access_time"]
+
+            # If the elapsed time is less than an hour, return a rate limit error
+            if elapsed_time < CACHE_EXPIRATION:
+                error_message = f"Rate limit exceeded. Maximum {RATE_LIMIT} requests per hour."
+                return JSONResponse(status_code=429, content={"error": error_message})
+
+            # If an hour has passed, reset the request count and update the first access time
+            cache[ip_address]["request_count"] = 1
+            cache[ip_address]["first_access_time"] = current_time
+
+    response = await call_next(request)
+
+    return response
+
+def clear_expired_cache():
+    current_time = time.time()
+    expired_entries = []
+
+    for ip_address, entry in cache.items():
+        elapsed_time = current_time - entry["first_access_time"]
+        if elapsed_time > CACHE_EXPIRATION:
+            expired_entries.append(ip_address)
+
+    for ip_address in expired_entries:
+        cache.pop(ip_address, None)
+
+
+async def clear_cache_periodically():
+    while True:
+        clear_expired_cache()
+        await asyncio.sleep(CACHE_EXPIRATION)  # Run every hour
+
+@app.on_event("startup")
+async def startup_event():
+    asyncio.create_task(clear_cache_periodically())
 
 app.include_router(api_router, prefix='/api/v1')
 handler = Mangum(app)
